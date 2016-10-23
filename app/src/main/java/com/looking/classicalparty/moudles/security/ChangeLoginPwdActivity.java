2a8f33767a7d9cd35eb8fefc0774d53b6a79ca47
“userblythe104 <blythe104@github.com>
2016-10-23 18:36:23 +0800

    security

---
+++ app/src/main/java/com/looking/classicalparty/moudles/security/ChangeLoginPwdActivity.java
@@ -0,0 +1,31 @@
+package com.looking.classicalparty.moudles.security;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.looking.classicalparty.lib.base.activity.BaseActivity;
+
+public class ChangeLoginPwdActivity extends BaseActivity {
+
+
+    @Override
+    public void initView() {
+        setContentView(R.layout.activity_change_login_pwd);
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void processClick(View v) {
+
+    }
+}
---
+++ app/src/main/java/com/looking/classicalparty/moudles/security/SecuritySettingActivity.java
@@ -0,0 +1,37 @@
+package com.looking.classicalparty.moudles.security;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.looking.classicalparty.lib.base.activity.BaseActivity;
+import com.looking.classicalparty.lib.widget.CustomerMenuView;
+
+public class SecuritySettingActivity extends BaseActivity {
+
+
+    private CustomerMenuView mCustomSecurity;
+
+    @Override
+    public void initView() {
+        setContentView(R.layout.activity_security_setting);
+        mCustomSecurity = (CustomerMenuView) findViewById(R.id.custom_security);
+        // TODO: 2016/10/23 图片需要更换
+        mCustomSecurity.addDivider().addItem(R.mipmap.ic_person_msg, "密码修改", "change").addItem(R.mipmap.ic_person_msg, "清除缓存", "del").addItem(R.mipmap.ic_person_msg, "去评分", "score").build();
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void processClick(View v) {
+
+    }
+}
