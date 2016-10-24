5333f77ee290face98245330b918e4defd196ca6
blythe104 <blythe104@github.com>
2016-10-24 09:13:50 +0800

    添加注册

--- app/src/main/java/com/looking/classicalparty/moudles/register/view/RegisterActivity.java
+++ app/src/main/java/com/looking/classicalparty/moudles/register/view/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.looking.classicalparty.moudles.register.view;
 
+import android.content.Intent;
 import android.view.View;
+import android.widget.TextView;
 
 import com.looking.classicalparty.R;
 import com.looking.classicalparty.lib.base.activity.BaseActivity;
@@ -8,13 +10,17 @@ import com.looking.classicalparty.lib.base.activity.BaseActivity;
 public class RegisterActivity extends BaseActivity {
 
 
+    private TextView mTvNext;
+
     @Override
     public void initView() {
         setContentView(R.layout.activity_register);
+        mTvNext = (TextView) findViewById(R.id.tv_next);
     }
 
     @Override
     public void initListener() {
+        mTvNext.setOnClickListener(this);
 
     }
 
@@ -25,6 +31,11 @@ public class RegisterActivity extends BaseActivity {
 
     @Override
     public void processClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_next:
+                startActivity(new Intent(this, RegisterDetailActivity.class));
+                break;
+        }
 
     }
 }
