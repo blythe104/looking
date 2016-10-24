7739551f6ed33fa0fd05b2f1faf4afd3581d3fb0
“userblythe104 <blythe104@github.com>
2016-10-24 22:20:33 +0800

    add feedback

--- app/src/main/java/com/looking/classicalparty/moudles/feedback/FeedBackActivity.java
+++ app/src/main/java/com/looking/classicalparty/moudles/feedback/FeedBackActivity.java
@@ -1,15 +1,41 @@
 package com.looking.classicalparty.moudles.feedback;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.looking.classicalparty.R;
 import com.looking.classicalparty.lib.base.activity.BaseActivity;
 
 public class FeedBackActivity extends BaseActivity {
 
+    private EditText mEtFeedback;
+    private TextWatcher watcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
+    private TextView mTvTextNum;
+
     @Override
     public void initView() {
         setContentView(R.layout.activity_feed_back);
+        mEtFeedback = (EditText) findViewById(R.id.et_feedback);
+        mEtFeedback.addTextChangedListener(watcher);
+        mTvTextNum = (TextView) findViewById(R.id.tv_textnum);
     }
 
     @Override
